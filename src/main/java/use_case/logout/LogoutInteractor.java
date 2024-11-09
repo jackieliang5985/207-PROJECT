6003cad0dd75210ced179c49c8f02a6a2c06ed22
Ujivl <90654578+Ujivl@users.noreply.github.com>
2024-11-09 16:31:30 -0500

    added sample code for user log in and also made it compatible with current project env

---
+++ src/main/java/use_case/logout/LogoutInteractor.java
@@ -0,0 +1,24 @@
+package use_case.logout;
+
+/**
+ * The Logout Interactor.
+ */
+public class LogoutInteractor implements LogoutInputBoundary {
+    private LogoutUserDataAccessInterface userDataAccessObject;
+    private LogoutOutputBoundary logoutPresenter;
+
+    public LogoutInteractor(LogoutUserDataAccessInterface userDataAccessInterface,
+                            LogoutOutputBoundary logoutOutputBoundary) {
+        this.userDataAccessObject = userDataAccessInterface;
+        this.logoutPresenter = logoutOutputBoundary;
+    }
+
+    @Override
+    public void execute(LogoutInputData logoutInputData) {
+        final String username = logoutInputData.getUsername();
+        userDataAccessObject.setCurrentUsername(null);
+        final LogoutOutputData logoutOutputData = new LogoutOutputData(username, false);
+        logoutPresenter.prepareSuccessView(logoutOutputData);
+    }
+}
+
