c0489ff60b730f8bbeb7158c57b6f6b0c73b94d0
Ethan Chiu <ethanchiu0212@gmail.com>
2024-11-25 15:07:07 -0500

    revised entities

---
+++ src/main/java/entity/PostNoteEntity.java
@@ -0,0 +1,40 @@
+package entity;
+
+/**
+ * Represents an individual post-it note on the panel.
+ */
+public class PostNoteEntity {
+    private int x;
+    private int y;
+    private int width;
+    private int height; // initializes the position and size of the postit note
+    private MindMap mindMap; // Reference to the mind map so we can repaint
+
+    public PostNoteEntity(int x, int y, int width, int height, MindMap mindMap) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.mindMap = mindMap;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public MindMap getMindMap() {
+        return mindMap;
+    }
+}
---
+++ src/main/java/entity/TextPostNoteEntity.java
@@ -0,0 +1,25 @@
+package entity;
+
+import java.awt.Color;
+
+/**
+ * Represents an individual text post-it note on the panel.
+ */
+public class TextPostNoteEntity extends PostNoteEntity {
+    private Color color; // Color of the post-it note
+    private String text; // Text field for editing the post-it label
+
+    public TextPostNoteEntity(int x, int y, int width, int height, Color color, MindMap mindMap) {
+        super(x, y, width, height, mindMap);
+        this.color = color;
+        this.text = "";
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
