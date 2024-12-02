69b80a946b1c6695cd3268a96a8ff4c9b92a6e94
sudharshanpal <sudharshanpalaniyappan12@gmail.com>
2024-12-01 22:48:43 -0500

    refactored naming

--- src/main/java/use_case/add_Image_PostNote/ImagePostNoteInteractor.java
+++ src/main/java/use_case/add_Image_PostNote/ImagePostNoteInteractor.java
@@ -7,21 +7,20 @@ import entity.MindMapEntity;
 
 public class ImagePostNoteInteractor implements ImagePostNoteInputBoundary {
     private final ImagePostNoteOutputBoundary outputBoundary;
-    private final PostNoteDataAccessInterface postNoteDAO;
+    private final PostNoteDataAccessInterface postNoteDataAccessInterface;
     private final MindMapEntity mindMapEntity;
 
     // Constructor
     public ImagePostNoteInteractor(ImagePostNoteOutputBoundary outputBoundary,
-                                   PostNoteDataAccessInterface postNoteDAO, MindMapEntity mindMapEntity) {
+                                   PostNoteDataAccessInterface postNoteDataAccessInterface, MindMapEntity mindMapEntity) {
         this.outputBoundary = outputBoundary;
-        this.postNoteDAO = postNoteDAO;
+        this.postNoteDataAccessInterface = postNoteDataAccessInterface;
         this.mindMapEntity = mindMapEntity;
     }
 
     @Override
     public void addImagePostNote(ImagePostNoteInputData inputData) {
-        // Create an ImagePostNoteEntity instead of a PostNoteEntity
-        ImagePostNoteEntity imagePostNoteEntity = new ImagePostNoteEntity(
+        final ImagePostNoteEntity imagePostNoteEntity = new ImagePostNoteEntity(
                 inputData.getX(),
                 inputData.getY(),
                 inputData.getWidth(),
@@ -29,11 +28,7 @@ public class ImagePostNoteInteractor implements ImagePostNoteInputBoundary {
                 mindMapEntity,
                 inputData.getImageUrl()
         );
-
-        // Add the ImagePostNoteEntity to the DAO (data persistence)
-        postNoteDAO.addPostNote(imagePostNoteEntity);
-
-        // Pass data to the presenter (output boundary)
+        postNoteDataAccessInterface.addPostNote(imagePostNoteEntity);
         outputBoundary.presentImagePostNotes(new ImagePostNoteData(inputData.getImageUrl(),
                 inputData.getX(), inputData.getY(), inputData.getColor()));
     }
